85135129929bbb2b65d89dabd978e69aead0314a
m tahseen <tahseenalam345@gmail.com>
2026-03-05 12:52:51 +0500

    m

--- src/components/FacebookPixel.tsx
+++ src/components/FacebookPixel.tsx
@@ -1,38 +1,49 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
 import * as fbq from "@/lib/fpixel";
 
-export default function FacebookPixel() {
+// 🚀 FIX: We separate the hook logic into its own component...
+function PixelEvents() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  // Track PageView on route change
   useEffect(() => {
     fbq.pageview();
   }, [pathname, searchParams]);
 
+  return null;
+}
+
+export default function FacebookPixel() {
   if (!fbq.FB_PIXEL_ID) return null;
 
   return (
-    <Script
-      id="fb-pixel"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${fbq.FB_PIXEL_ID}');
-        `,
-      }}
-    />
+    <>
+      {/* 🚀 FIX: ...and wrap it in Suspense to satisfy Vercel's build requirements */}
+      <Suspense fallback={null}>
+        <PixelEvents />
+      </Suspense>
+      
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${fbq.FB_PIXEL_ID}');
+          `,
+        }}
+      />
+    </>
   );
 }
