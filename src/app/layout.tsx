ce90816ea9e5a9cd2aaa423cc8a606bd3a2d0635
m tahseen <tahseenalam345@gmail.com>
2026-03-10 11:47:01 +0500

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
               
               // 👇 REPLACE THIS WITH YOUR REAL TIKTOK PIXEL ID 👇
-              ttq.load(D6NRJURC77U3SAC8BIFG);
+              ttq.load('D6NRJURC77U3SAC8BIFG');
               ttq.page();
             }(window, document, 'ttq');
           `}
